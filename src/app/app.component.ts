419cc233ee7fe615fcbb9a96d2738432dc8ac75f
sreelathas81 <56813315+sreelathas81@users.noreply.github.com>
2020-03-05 12:55:05 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as data from'../app/data/data.json';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+  pStatus=false;
+  ppStatus;
+  nstatus;
+  info=(data as any).default;
+  public id:number=0;
+  public iidd:number;
+  constructor()
+  {
+  }
+   
+  save(b)
+  {
+    this.id=b;
+    console.log(b);
+    if(b==1){
+      this.pStatus=true;
+      this.nstatus=false;
+      }if(b==10){
+        this.nstatus=true;
+        this.pStatus=false;
+      }
+    }
+    prev(){
 
-pStatus=false;
-info=(data as any).default;
-
-  id:any;
-  
-save(b){
-  this.id=b;
-}
+    }
+    next(){
+      
+    }
+  }
 
 
-}
+
+
+
+
+
