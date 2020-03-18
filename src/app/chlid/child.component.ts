d13796aa0cb3378ab36e861fe6c402ddc74bd1e9
sreelathas81 <56813315+sreelathas81@users.noreply.github.com>
2020-03-18 18:25:35 +0530

    Add files via upload

---
+++ src/app/chlid/child.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {HttpHeaders} from '@angular/common/http';
+import  * as dd from 'rxjs/add/operator/toPromise';
+
+
+
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Output() data:any=new EventEmitter();
+@Output()showSpinner=new EventEmitter();
+constructor(private http:HttpClient) {}
+info:any = [];
+id:number;
+private  headers=new HttpHeaders({'Content-type':'application/json'});  
+
+
+  ngOnInit() {
+  this.http.get("http://localhost:5555/data").subscribe((res:Response)=>{
+    this.info=res;
+  })
+     /* this.api.getdata()
+      .subscribe(data => {
+        this.info=data;
+        console.log(data);
+      });*/
+    
+  
+  
+}
+showData(a,c){
+  this.data.emit(a);
+  this.showSpinner.emit(c);
+}
+dele(aa){
+const url=`${"http://localhost:5555/data"}/${aa}`;
+ this.http.delete(url,{headers:this.headers}).subscribe(()=>{
+    this.info = this.info.filter(item=>item.id !=aa );
+
+  }
+
+  )
+  
+  
+
+}
+
+
+}
