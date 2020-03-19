51a5db5cd162481e3b6e92e39e7910059c37eab6
sreelathas81 <56813315+sreelathas81@users.noreply.github.com>
2020-03-19 11:48:00 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChlidComponent } from './chlid/chlid.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChildComponent } from './child/child.component';
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatRadioModule} from '@angular/material/radio';
+import{PapaParseModule} from 'ngx-papaparse';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChlidComponent
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PDFExportModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    MatRadioModule,
+    PapaParseModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
